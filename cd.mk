2e2f782c88d86101ae5e08cd2d548c54db97664e
yanniks <kontakt@yanniks.de>
2013-09-15 13:41:13 +0200

    changed prefix from cm to cd
    Conflicts:
    	cd.mk

--- cd.mk
+++ cd.mk
@@ -8,5 +8,5 @@ $(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7wlv/m7wlv:4.2.2/JDQ39/200197.1:user/release-keys" PRIVATE_BUILD_DESC="1.10.605.8 CL221778 release-keys"
 
-PRODUCT_NAME := cm_m7vzw
+PRODUCT_NAME := cd_m7vzw
 PRODUCT_DEVICE := m7vzw
